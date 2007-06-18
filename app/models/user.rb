a0cbe3e28c843d37a7b352a6015f08c5fa12adba
Brendan Taylor <whateley@gmail.com>
2007-06-17 22:55:07 -0600

    used OpenidLoginGenerator
    darcs-hash:20070618045507-ce558-ec0acbebbaa5778ff8721da7e1aa092fea6f8472.gz

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
+
+# this model expects a certain database layout and its based on the name/login pattern. 
 class User < ActiveRecord::Base
   def collections
     Collection.find_all_by_user_id(self.id)
   end
-
+ 
   def auth_for abs_url, realm
     HTTPAuth.find :first, :conditions => [ 'user_id = ? and abs_url = ? and realm = ?', self.id, abs_url, realm ]
   end
+
+  def self.get(openid_url)
+    find_first(["openid_url = ?", openid_url])
+  end  
+  
+  protected
+  
+  validates_uniqueness_of :openid_url, :on => :create
+  validates_presence_of :openid_url
 end
