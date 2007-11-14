443a98606bfc3735032033d3d58ffab33f594ea3
Brendan Taylor <whateley@gmail.com>
2007-11-14 11:45:32 -0700

    recover from Unauthorized when adding a collection
    darcs-hash:20071114184532-ce558-a5550bd781c448939c0caa26d67ad336e7962da9.gz

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -20,41 +20,46 @@ class WallController < ApplicationController
     @coll_url = "http://" + @coll_url unless @coll_url.match(/^http/)
 
     http = new_atom_http
-    res = http.get(@coll_url, "Accept" => "application/atom+xml;type=feed, application/atomsvc+xml, application/xhtml+xml;q=0.6, text/html;q=0.5")
 
-    if res.code == '200'
-      case res["Content-Type"]
-      when /application\/atom\+xml/
-        @collection = Collection.new(:url => @coll_url, :user_id => @user)
+    begin
+      res = http.get(@coll_url, "Accept" => "application/atom+xml;type=feed, application/atomsvc+xml, application/xhtml+xml;q=0.6, text/html;q=0.5")
 
-        if @collection.save
-          flash[:notice] = 'Collection was successfully created.'
-          redirect_to :controller => 'collection', :action => 'show', :url => @collection.url
-        else
-          raise "couldn't save collection"
-        end
-      when /application\/atomsvc\+xml/
-        @service = Atom::Service.parse(res.body)
+      if res.code == '200'
+        case res["Content-Type"]
+        when /application\/atom\+xml/
+          @collection = Collection.new(:url => @coll_url, :user_id => @user)
 
-        @collections = []
-
-        @service.collections.each do |acoll|
-          coll = Collection.new(:url => acoll.uri.to_s, :user_id => @user)
-          unless coll.save
-            raise coll.errors.inspect
+          if @collection.save
+            flash[:notice] = 'Collection was successfully created.'
+            redirect_to :controller => 'collection', :action => 'show', :url => @collection.url
+          else
+            raise "couldn't save collection"
           end
-          @collections << coll
-        end
+        when /application\/atomsvc\+xml/
+          @service = Atom::Service.parse(res.body)
 
-        flash[:notice] = "i added the collections i found there."
-        redirect_to :action => 'show'
-      when /text\/html/, /application\/xhtml\+xml/
-        raise "collection autodiscovery from HTML not yet implemented"
+          @collections = []
+
+          @service.collections.each do |acoll|
+            coll = Collection.new(:url => acoll.uri.to_s, :user_id => @user)
+            unless coll.save
+              raise coll.errors.inspect
+            end
+            @collections << coll
+          end
+
+          flash[:notice] = "i added the collections i found there."
+          redirect_to :action => 'show'
+        when /text\/html/, /application\/xhtml\+xml/
+          raise "collection autodiscovery from HTML not yet implemented"
+        else
+          raise "i don't know how to get a collection from a document of type #{res['Content-Type']}"
+        end
       else
-        raise "i don't know how to get a collection from a document of type #{res['Content-Type']}"
+        raise "#{@coll_url} responded with unexpected status: #{res.code}"
       end
-    else
-      raise "#{@coll_url} responded with unexpected status: #{res.code}"
+    rescue Atom::Unauthorized
+      obtain_authorization(:post, 'collection[url]' => @coll_url)
     end
   end
 
