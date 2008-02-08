11801f6db1337412f4d7f1b6dfe0ee416b1b273b
Brendan Taylor <whateley@gmail.com>
2008-02-07 19:00:08 -0700

    update to ruby-openid 2.0.3
    darcs-hash:20080208020008-ce558-945b08a04e31e86cc84080c7a4685d31dd24be0d.gz

--- app/controllers/openid_controller.rb
+++ app/controllers/openid_controller.rb
@@ -2,12 +2,12 @@ require "pathname"
 require "cgi"
 
 # load the openid library
-begin
-  require "rubygems"
-  gem "openid", ">= 1.0.2"
-rescue LoadError
-  require "openid"
-end
+require "rubygems"
+
+gem "ruby-openid", ">= 2.0.0"
+
+require 'openid/store/filesystem'
+require 'openid/consumer'
 
 class OpenidController < ApplicationController
   # process the login request, disover the openid server, and
@@ -20,33 +20,36 @@ class OpenidController < ApplicationController
         request = consumer.begin(openid_url)
       rescue Timeout::Error
         flash[:notice] = "could not connect with OpenID server"
-      else
-        case request.status
-        when OpenID::SUCCESS
-          return_to = url_for(:action=> 'complete')
-          trust_root = url_for(:controller => 'static', :action => 'index')
 
-          url = request.redirect_url(trust_root, return_to)
-          redirect_to(url)
-          return
-        when OpenID::FAILURE
-          escaped_url = CGI.escapeHTML(openid_url)
-          flash[:notice] = "Could not find OpenID server for #{escaped_url}"
-        else
-          flash[:notice] = "An unknown error occured."
-        end
+        redirect_to :controller => "static", :action => "index"
+
+        return
+      rescue OpenID::OpenIDError
+        escaped_url = CGI.escapeHTML(openid_url)
+        flash[:notice] = "Could not find OpenID server for #{escaped_url}"
+
+        redirect_to :controller => "static", :action => "index"
+
+        return
       end
 
-      redirect_to :controller => "static", :action => "index"
+      return_to = url_for :action => 'complete'
+      trust_root = url_for :controller => 'static', :action => 'index'
+
+      url = request.redirect_url(trust_root, return_to)
+
+      redirect_to url
     end
   end
 
   # handle the openid server response
   def complete
-    response = consumer.complete(params)
+    current_url = url_for :action => 'complete', :only_path => false
+    parameters = params.reject{|k,v|request.path_parameters[k]}
+    response = consumer.complete(parameters, current_url)
 
     case response.status
-    when OpenID::SUCCESS
+    when OpenID::Consumer::SUCCESS
       @user = User.find_or_initialize_by_openid_url(response.identity_url)
 
       if @user.new_record?
@@ -59,14 +62,14 @@ class OpenidController < ApplicationController
       redirect_to :controller => "user", :action => "show"
 
       return
-    when OpenID::FAILURE
+    when OpenID::Consumer::FAILURE
       if response.identity_url
         escaped_url = CGI.escapeHTML(response.identity_url)
         flash[:notice] = "OpenID verification for #{escaped_url} failed."
       else
         flash[:notice] = 'OpenID verification failed.'
       end
-    when OpenID::CANCEL
+    when OpenID::Consumer::CANCEL
       flash[:notice] = 'OpenID verification cancelled.'
     else
       flash[:notice] = 'Unknown response from OpenID server.'
@@ -90,7 +93,7 @@ class OpenidController < ApplicationController
     # Note: see the plugin located at examples/active_record_openid_store 
     # if you need to store this information in your database. 
     store_dir = Pathname.new(RAILS_ROOT).join('db').join('openid-store')
-    store = OpenID::FilesystemStore.new(store_dir)
+    store = OpenID::Store::Filesystem.new(store_dir)
 
     return OpenID::Consumer.new(session, store)
   end
