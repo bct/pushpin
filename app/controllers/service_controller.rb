ff1d2843af03facff003a1887811e9303dd6bc81
Brendan Taylor <whateley@gmail.com>
2008-02-20 21:30:28 -0700

    use new service autodiscovery
    darcs-hash:20080221043028-ce558-689d4aebf23221b59ca8fadc31d68c18a763edc9.gz

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,9 +1,9 @@
 class ServiceController < ApplicationController
   def show
-    @serv_url = n_url params[:url]
+    serv_url = n_url params[:url]
 
-    maybe_needs_authorization('url' => @serv_url) do
-      @service = Atom::Service.new @serv_url, new_atom_http
+    maybe_needs_authorization('url' => serv_url) do
+      @service = Atom::Service.discover serv_url, new_atom_http
     end
   end
 end
